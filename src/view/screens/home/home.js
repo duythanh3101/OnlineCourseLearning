879d48f3a9de831df14231a846009d86dc222e75
duythanhphm <duythanh3101@gmail.com>
2020-05-07 02:14:00 +0700

    Add some necessary packages and architecture of project

---
+++ src/view/screens/home/home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const home = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default home
+
+const styles = StyleSheet.create({
+    
+})
